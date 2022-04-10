1812ee44a131a4185f060580f29aacc4b31dbedb
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-04-09 23:52:52 -0600

    the basics

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 // GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
+
+
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
 // WHEN I click into a time block
@@ -12,6 +10,15 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 
+var hour = "9:00";
+var timeBlock = document.querySelector("#hourbox")
 
-moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(moment());
+//DONE
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+var dateDisplay = document.querySelector("#currentDay")
+dateDisplay.textContent = moment().format('MMMM Do YYYY');
+
+//in progress
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours
