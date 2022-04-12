8e50da55ec92d5fdaa7b31f73c4cf3974255de95
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-04-12 09:14:20 -0600

    added classlist function to textarea

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,18 @@
 var dateDisplay = document.querySelector("#currentDay")
 dateDisplay.textContent = moment().format('MMMM Do YYYY');
 //current date
-var CurrentDate = moment().format('MMMM Do YYYY');
+var currentDate = moment().format('MMMM Do YYYY');
 //current hour 12 hour format
-var CurrentTime = moment().hours();
+var currentTime = 12;
+var text1 = document.querySelector("#text-status1");
+var text2 = document.querySelector("#text-status2");
+var text3 = document.querySelector("#text-status3");
+var text4 = document.querySelector("#text-status4");
+var text5 = document.querySelector("#text-status5");
+var text6 = document.querySelector("#text-status6");
+var text7 = document.querySelector("#text-status7");
+var text8 = document.querySelector("#text-status8");
+var text9 = document.querySelector("#text-status9");
 //done
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
@@ -17,11 +26,12 @@ var taskBox1 = document.querySelector("#timeoftask1").textContent = taskTime;
 var taskBox2 = document.querySelector("#timeoftask2").textContent = taskTime+1;
 var taskBox3 = document.querySelector("#timeoftask3").textContent = taskTime+2; 
 var taskBox4 = document.querySelector("#timeoftask4").textContent = taskTime+3; 
-var taskBox5 = document.querySelector("#timeoftask5").textContent = taskTime-8; 
-var taskBox6 = document.querySelector("#timeoftask6").textContent = taskTime-7; 
-var taskBox7 = document.querySelector("#timeoftask7").textContent = taskTime-6; 
-var taskBox8 = document.querySelector("#timeoftask8").textContent = taskTime-5; 
-var taskBox9 = document.querySelector("#timeoftask9").textContent = taskTime-4; 
+var taskBox5 = document.querySelector("#timeoftask5").textContent = taskTime+4; 
+var taskBox6 = document.querySelector("#timeoftask6").textContent = taskTime+5; 
+var taskBox7 = document.querySelector("#timeoftask7").textContent = taskTime+6; 
+var taskBox8 = document.querySelector("#timeoftask8").textContent = taskTime+7; 
+var taskBox9 = document.querySelector("#timeoftask9").textContent = taskTime+8;
+
 //adding am and pm
 document.querySelector("#timeoftask1").textContent = taskBox1 + "AM";
 document.querySelector("#timeoftask2").textContent = taskBox2 + "AM";
@@ -33,16 +43,80 @@ document.querySelector("#timeoftask7").textContent = taskBox7 + "PM";
 document.querySelector("#timeoftask8").textContent = taskBox8 + "PM";
 document.querySelector("#timeoftask9").textContent = taskBox9 + "PM";
 
-
+console.log(taskBox1 + "this is taskbox1 time");
 //in progress
 
 // var taskStatus = document.querySelector("#text-status")
 
 var taskStatus = function() {
-    if (CurrentTime > taskBox1) {
-        taskBox1.classList.add("past");
-        console.log("task 1 is past due and should be grey")
-    }
+    //working classlist function to change colors based on time
+    //todo: replace time var with momentjs function code
+    if (currentTime > taskTime) {
+        text1.classList.add("past");
+        } else if (currentTime === taskTime) {
+            text1.classList.add("present")
+        } else (currentTime < taskTime) 
+            text1.classList.add("future");
+        }
+    console.log(currentTime, taskTime);
+    // if (currentTime > taskTime++) {
+    //     text2.classList.add("past");
+    //     } else if (currentTime === taskTime++) {
+    //         text2.classList.add("present")
+    //     } else {
+    //     text2.classList.add("future");
+    // } 
+    // if (currentTime > taskTime) {
+    //     text1.classList.add("past");
+    //     } else if (currentTime === taskTime) {
+    //         text1.classList.add("present")
+    //     } else {
+    //     text1.classList.add("future");
+    // } 
+    // if (currentTime > taskTime) {
+    //     text1.classList.add("past");
+    //     } else if (currentTime === taskTime) {
+    //         text1.classList.add("present")
+    //     } else {
+    //     text1.classList.add("future");
+    // } 
+    // if (currentTime > taskTime) {
+    //     text1.classList.add("past");
+    //     } else if (currentTime === taskTime) {
+    //         text1.classList.add("present")
+    //     } else {
+    //     text1.classList.add("future");
+    // } 
+    // if (currentTime > taskTime) {
+    //     text1.classList.add("past");
+    //     } else if (currentTime === taskTime) {
+    //         text1.classList.add("present")
+    //     } else {
+    //     text1.classList.add("future");
+    // } 
+    // if (currentTime > taskTime) {
+    //     text1.classList.add("past");
+    //     } else if (currentTime === taskTime) {
+    //         text1.classList.add("present")
+    //     } else {
+    //     text1.classList.add("future");
+    // } 
+    // if (currentTime > taskTime) {
+    //     text1.classList.add("past");
+    //     } else if (currentTime === taskTime) {
+    //         text1.classList.add("present")
+    //     } else {
+    //     text1.classList.add("future");
+    // } 
+    // if (currentTime > taskTime) {
+    //     text1.classList.add("past");
+    //     } else if (currentTime === taskTime) {
+    //         text1.classList.add("present")
+    //     } else {
+    //     text1.classList.add("future");
+    // } 
+    
+    
     
     //turn grey if getcurrenttime is > hour box
 
@@ -54,11 +128,9 @@ var taskStatus = function() {
     //     console.log("getcurtime is greater than timeblock");
     // }
 
-}
+
 taskStatus();
 
-console.log(CurrentTime + " this is the current hour");
-console.log(taskBox1);
 
 
 // WHEN I view the time blocks for that day
